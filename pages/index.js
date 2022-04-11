798756c2090068e6c3353908e89d8b0c544e8c77
twipzzz <dmitriygumz@gmail.com>
2022-04-11 19:52:30 +0300

    firebase setup

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,18 @@ import styles from './index.module.scss';
 
 import Cards from 'components/HomeCard/cards';
 import Products from 'components/HomeProducts';
+import { db } from 'config/firebase';
 
 export default function Home() {
+  db.collection('Products')
+    .get()
+    .then(function (querySnapshot) {
+      querySnapshot.forEach(function (doc) {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, ' => ', doc.data());
+      });
+    });
+
   return (
     <div className={styles.container}>
       <Head>
