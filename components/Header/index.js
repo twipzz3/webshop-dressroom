492a8e7655d2c47a8d2b732f2790c50ba42290e7
twipzzz <dmitriygumz@gmail.com>
2022-04-17 13:37:56 +0300

    add dropdown menu in header for login/register

--- components/Header/index.js
+++ components/Header/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Link from 'next/link';
 
 import styles from './header.module.scss';
+import ArrowIcon from '@/icons/arrow';
 
-export default function Header() {
+export default function Header({ isSigned }) {
   return (
     <nav className={styles.container}>
       <Link href="/">
@@ -20,6 +21,32 @@ export default function Header() {
         <Link href="/">
           <div className={styles.cartContainer}>
             <span>CART (0)</span>
+          </div>
+        </Link>
+        <Link href="/">
+          <div className={styles.profileContainer}>
+            <span>
+              HELLO <span style={{ fontWeight: 'normal' }}>GUEST</span>
+            </span>
+            <ArrowIcon width={12} height={12} className={styles.arrowIcon} />
+            <div className={styles.dropdown}>
+              <div className={styles.arrowUp} />
+              <div className={styles.dropdownMenu}>
+                {isSigned ? (
+                  <>
+                    <Link href="/orders">My Orders</Link>
+                    <Link href="/account">My Account</Link>
+                    <Link href="/favorites">Favourites</Link>
+                    <Link href="/">Logout</Link>
+                  </>
+                ) : (
+                  <>
+                    <Link href="/login">Login</Link>
+                    <Link href="/register">Register</Link>
+                  </>
+                )}
+              </div>
+            </div>
           </div>
         </Link>
       </div>
